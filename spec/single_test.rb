7dba782bd83acd8315d380275723bd15236c0de4
eigencodel <marcelbutucea@gmail.com>
2017-10-19 13:54:41 +0800

    Update single_test.rb

--- spec/single_test.rb
+++ spec/single_test.rb
@@ -3,9 +3,7 @@ require_relative '../scripts/browserstack.rb'
 describe "Load Front Page" do
   it "can find search results" do
     @driver.navigate.to "https://app.dragonlaw.io/"
-    element = @driver.find_element(:name, 'email')
-    element.send_keys "admin+newrelic@dragonlaw.com.hk"
-    sleep 1
+    sleep 5
     expect(@driver.title).to eql("Login - Dragon Law")
   end
 end
